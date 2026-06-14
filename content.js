a57f7eb60fa9f02d040dd682251bd16ba5698841
VDC-Digital <ptkrjf6b29@privaterelay.appleid.com>
2026-06-13 17:29:39 -0700

    Update content.js

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ const VOODAC_CONTENT = `
 [box2=More to Your Noon]
 <bg=#fe0033>
 There is now more to your noon as <b>Sunday at Noon is coming back</b>. Expect first video updates starting 6.21. You can view all S@N content online by visitng the <b>S@N Content Hub.</b><br><br>
-<size=7>Navigate to S@N Content Hub by going to Menu, then pressing the S@N Hub Link.
+<size=7>Navigate to S@N Content Hub by going to Menu, then pressing the S@N Hub Link.</size>
 </bg>
 [/box2]
 
