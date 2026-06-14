0e457d4b10a5d4931bfeff2994675f357e1e1210
VDC-Digital <ptkrjf6b29@privaterelay.appleid.com>
2026-06-13 17:24:23 -0700

    Revise VOODAC content for updates and clarity
    Updated VOODAC content with new sections and removed outdated information.

--- content.js
+++ content.js
@@ -1,83 +1,10 @@
 const VOODAC_CONTENT = `
 
-[box2=VOODAC Home Page]
-<bg=grey>
-<color=black>
-<center><b><hr>
-    The home for everything VOODAC. 
-</b></color></center><hr>
-
-Have questions or special requests? Explore our portfolio to see what VOODAC has achieved in precision and industrial solutions. Reach out today to request a custom solution or learn more about our capabilities.
+[box2=More to Your Noon]
+<bg=#fe0033>
+There is now more to your noon as <b>Sunday at Noon is coming back</b>. Expect first video updates starting 6.21. You can view all S@N content online by visitng the <b>S@N Content Hub.</b><br><br>
+<size=15>Navigate to S@N Content Hub by going to Menu, then pressing the S@N Hub Link.
 </bg>
 [/box2]
-
-[box1=Contact VOODAC]
-<bg=#0090FF>
-<center><b>Custom Solutions</b></center>
-<hr>
-Contact at <link=mailto:nexus44.solder@icloud.com>nexus44.solder@icloud.com</link>
-<br><br>
-<center><b>Questions</b></center>
-<hr>
-Questions at <link=mailto:uplift.krill_0q@icloud.com>uplift.krill_0q@icloud.com</link>
-</bg>
-[/box1]
-
-[box2=The new VOODAC]
-This is the new VOODAC<br>This is cool because it has line breaks<br>hello
-[/box2]
-
-[box1=June–July Forecast]
-- Field test station to resume production after 6.19. <br>
-- Material mixer R&D to begin after 6.13. <br>
-- Strategic business planning on 6.21. <br>
-- YouTube channel for introductions.
-[/box1]
-
-[box1=Navigating this Application]
-<bg=#82D35F>
-This is the <b>home page</b>.<br>
-To access the <b>menu</b>, tap (or click) the <b>VOODAC logo</b> in the top right-hand corner.
-</bg>
-[/box1]
-
-[box1=Video Updates]
-<bg=#FF0033>
-Visit the VOODAC <link=http://www.youtube.com/@voodac>YouTube</link> channel for live events and announcements.
-</bg>
-[/box1]
-
-[video1=https://www.youtube.com/embed/dQw4w9WgXcQ]
-
-[box2=System Updates]
-<bg=#FF4C4B>
-There will be a significant update to this application as it is released to more of the public. In this update, expect to see an automated system that updates this application without manual intervention.
-</bg>
-[/box2]
-
-[cat=VOODAC Location]
-VOODAC is a Canadian company located in the lower mainland of British Columbia. VOODAC specializes in creating precision technologies and industrial equipment designed to handle the harsh conditions of the B.C. rainforest weather.
-[/cat]
-
-[cat=Rights]
-There is no trademark or copyright filed for the name or logo VOODAC. This is original property and all original files with metadata are kept with date. Use of this name and logo is highly discouraged.
-[/cat]
-
-[cat=Software Version]
-This is the first official version of the application. This is the 4th conceptual version of the application. Application was designed by creator with the use of AI with minor operations and details changed by hand.
-[/cat]
-
-[cat=Master Contact List]
-- Solutions: nexus44.solder@icloud.com<br>
-- Questions: uplift.krill_0q@icloud.com<br>
-- Personal: ryan.worden.email@gmail.com<br>
-- UPD_SYS: <mono>undefined</mono>
-[/cat]
-
-[menulink=YouTube]http://www.youtube.com/@voodac[/menulink]
-[menulink=Email Solutions]mailto:nexus44.solder@icloud.com[/menulink]
-[menulink=Email Questions]mailto:uplift.krill_0q@icloud.com[/menulink]
-
-[footer]© 2025 VOODAC. All rights reserved. Original property of VOODAC. No trademark filed — use of this name and logo is highly discouraged. Located in British Columbia, Canada.[/footer]
 
 `;
