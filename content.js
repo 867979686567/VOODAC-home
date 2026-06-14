6bb33ffd59216d60c1a7a7afa7722365bbb9c4c4
VDC-Digital <ptkrjf6b29@privaterelay.appleid.com>
2026-06-13 17:37:14 -0700

    Update content.js with new footer and formatting

--- content.js
+++ content.js
@@ -3,10 +3,11 @@ const VOODAC_CONTENT = `
 [box2=More to Your Noon]
 <bg=#fe0033>
 There is now more to your noon as <b>Sunday at Noon is coming back</b>. Expect first video updates starting 6.21. You can view all S@N content online by visitng the <b>S@N Content Hub.</b><br><br>
-<size=2>Navigate to S@N Content Hub by going to Menu, then pressing the S@N Hub Link.</size>
+Navigate to S@N Content Hub by going to Menu, then pressing the S@N Hub Link.<br><br>
+<center><i>- Sunday at Noon</i></center>
 </bg>
 [/box2]
 
-
+[footer]Copyright © 2026 VOODAC Digital. VOODAC Digital is a trademark of VOODAC. | Canada[/footer] 
 
 `;
